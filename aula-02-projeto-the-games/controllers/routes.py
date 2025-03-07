b2089e888b32448a81ff2c6c4350a510e29472ca
dev-DanielSilva <dev-DanielSilva@outlook.com>
2025-03-07 11:49:09 -0300

    Adição de Cadastro

--- aula-02-projeto-the-games/controllers/routes.py
+++ aula-02-projeto-the-games/controllers/routes.py
@@ -1,7 +1,12 @@
 from flask import render_template, request
 
-jogadores = ["MiDNa", "davi_lambari",
-             "fanylinda", "suaIrmã", "edsonGf"]
+jogadores = ["MiDna", "davi_lambari",
+             "fanylinda", "SuaIrmã", "Iruah"]
+# Array de objetos
+gameList = [{'Título': 'The Legend of Zelda: Breath of the Wild',
+             'Ano': 2017,
+             'Categoria': 'Mundo Aberto'},
+            ]
 
 
 def init_app(app):
@@ -17,11 +22,8 @@ def init_app(app):
 
     @app.route('/games', methods=['GET', 'POST'])
     def games():
-
-        game = {'Título': 'The Legend of Zelda: Breath of the Wild',
-                'Ano': 2017,
-                'categoria': 'Mundo Aberto'}
-
+        # Acessando o primeiro jogo da lista de jogos
+        game = gameList[0]
         if request.method == 'POST':
             if request.form.get('jogador'):  # name do input
                 jogadores.append(request.form.get('jogador'))
@@ -30,3 +32,15 @@ def init_app(app):
                                game=game,
                                jogadores=jogadores
                                )
+    @app.route('/cadgames', methods=['GET', 'POST'])
+    def cadgames():
+        if request.method == "POST":
+            if request.form.get('titulo') and request.form.get('ano') and request.form.get('categoria'):
+                gameList.append({'Título': request.form.get('titulo'),
+                                 'Ano': request.form.get('ano'),
+                                 'Categoria': request.form.get('categoria')
+                                 })
+        return render_template('cadgames.html',
+                               gameList=gameList)
+        
+    
